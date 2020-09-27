43673a1511881d93f89d0378c8ad470c65099b60
liuxing <m2507@qq.com>
2020-09-27 21:39:47 +0800

    第四次分支测试提交

--- src/main/java/com/offcn/controller/TestController.java
+++ src/main/java/com/offcn/controller/TestController.java
@@ -18,6 +18,11 @@ public class TestController {
 
         System.out.println("测试提交");
     }
+
+    public  void  gittextfenzhi(){
+
+        System.out.println("测试分支提交");
+    }
     @GetMapping("/get")
     public String getSession(HttpSession session){
         return (String)session.getAttribute("msg");
